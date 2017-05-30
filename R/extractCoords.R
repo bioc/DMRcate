fdc36a10f8abbc354e87c3c2ff247b9cb392d217
Tim Peters <Tim.Peters@csiro.au>
2017-05-30 01:32:35 +0000

    Merge branch 'master' into release-3.5
    * master: (70 commits)
      DMRcate_1.12.1
      Update DMRcate-internal.Rd
      DMRcate_1.10.10
      Main function, dmrcate(), re-organised and sped up.
      Update cpg.annotate.R
      DMRcate_1.10.9, ty to Jenny Drnevich for bugspot
      Unloop promoters part of extractRanges.
      Faster version with no 'apply' calls.
      DMRcate_1.10.8
      DMRcate_1.10.7
      DMRcate_1.10.6, ty to Michael Prummer for bugspot
      DMRcate_1.10.5
      bracketfix
      DMRcate_1.10.4, ty to Xavier Pastor for patch
      DMRcate_1.10.3
      Messages in cpg.annotate()
      DMRcate_1.10.2
      DMRcate_1.10.2. Thank you to Yuan Tian and Eugene Wang for bugspotting
      1.10.1 LICENCE
      1.10.1 DESCRIPTION
      ...
    
    Conflicts:
    	DESCRIPTION
    	LICENSE
    	NEWS
    	R/DMR.plot.R
    	R/cpg.annotate.R
    	R/dmrcate.R
    	R/extractCoords.R
    	R/extractRanges.R
    	R/rmSNPandCH.R
    	man/DMRcate-internal.Rd
    	man/cpg.annotate.Rd
    	man/rmSNPandCH.Rd
    
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/branches/RELEASE_3_5/madman/Rpacks/DMRcate@129973 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/extractCoords.R
+++ R/extractCoords.R
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 #Thank you to Xavier Pastor from Bioconductor mailing list for this patch
 extractCoords <- function(xx)
 {
@@ -6,3 +7,19 @@ extractCoords <- function(xx)
     colnames(coords) <- c('chrom', 'chromStart', 'chromEnd')
     return(coords)
 }
+=======
+
+extractCoords <-
+  function(coords.M)
+  {
+    M <- length(coords.M)
+    A <- 3 # Should be three components
+    split <- "[:-]"
+    l <- strsplit(coords.M, split = split)
+    stopifnot(all(sapply(l, length) == A))
+    X.AM <- matrix(unlist(l), A, M)
+    df.MA <- data.frame(t(X.AM), stringsAsFactors = FALSE)
+    colnames(df.MA) <- c('chrom', 'chromStart', 'chromEnd')
+    df.MA
+  }
+>>>>>>> master
--- R/extractRanges.R
+++ R/extractRanges.R
@@ -1,27 +1,27 @@
-extractRanges <-function(dmrcoutput, genome=c("hg19", "hg38", "mm10")) 
+extractRanges <-function(dmrcoutput, genome=c("hg19", "hg38", "mm10"))
 {
   env <- new.env(parent=emptyenv())
   data(dmrcatedata, envir=env)
+  genome <- match.arg(genome)
   stopifnot(is(dmrcoutput, "dmrcate.output"))
   coords <- extractCoords(dmrcoutput$results$coord)
   coords <- cbind(coords, dmrcoutput$results[, c("no.cpgs", "minfdr", "Stouffer", "maxbetafc", "meanbetafc")])
   coords$chromStart <- as.integer(as.character(coords$chromStart))
   coords$chromEnd <- as.integer(as.character(coords$chromEnd))
   ranges <- makeGRangesFromDataFrame(coords, keep.extra.columns = TRUE)
-  switch(genome, 
+  switch(genome,
     hg19={tx=env$tx.hg19},
     hg38={tx=env$tx.hg38},
     mm10={tx=env$tx.mm10}
   )
   promsidx <- as.data.frame(findOverlaps(ranges, promoters(tx, 2000, 2000)))
   proms <- tapply(promsidx$subjectHits, promsidx$queryHits, function(x) tx[x])
-  ranges$overlapping.promoters <- NA
-  promcount <- 1
-  for(i in 1:length(ranges)){
-    if(i %in% as.numeric(names(proms))){
-      ranges$overlapping.promoters[i] <- paste(proms[[promcount]]$tx_name, collapse=", ")
-      promcount <- promcount + 1
-    }
-  }
+  op.A <- sapply(proms, function(l) paste(l$tx_name, collapse= ", "))
+  name.A <- names(proms)
+  m.A <- as.numeric(name.A)
+  M <- length(ranges)
+  overlapping.promoters <- rep(NA_character_, M)
+  overlapping.promoters[m.A] <- op.A
+  ranges$overlapping.promoters <- overlapping.promoters
   ranges
 }
